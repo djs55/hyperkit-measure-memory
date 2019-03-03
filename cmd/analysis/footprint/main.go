c4da52eee349a808ec6d51d108004f596b511738
David Scott <dave@recoil.org>
2019-03-03 14:30:24 +0000

    Graph hyperkit VSZ, RSS, footprint on 10.24
    Signed-off-by: David Scott <dave@recoil.org>

--- cmd/analysis/footprint/main.go
+++ cmd/analysis/footprint/main.go
@@ -8,20 +8,49 @@ import (
 )
 
 func main() {
-	points, err := sample.ReadDir("10.14-idle", func(s sample.Sample) int64 {
+	footprintPoints, err := sample.ReadDir("10.14-idle", func(s sample.Sample) int64 {
 		return int64(s.Footprint)
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	footprint := gnuplot.Line{
-		Label:  "physical footprint",
-		Points: points,
+	VSZPoints, err := sample.ReadDir("10.14-idle", func(s sample.Sample) int64 {
+		for _, command := range s.PS {
+			if command.Command == "com.docker.hyperkit" {
+				return command.VSZ
+			}
+		}
+		return int64(0)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	RSSPoints, err := sample.ReadDir("10.14-idle", func(s sample.Sample) int64 {
+		for _, command := range s.PS {
+			if command.Command == "com.docker.hyperkit" {
+				return command.RSS
+			}
+		}
+		return int64(0)
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 	g := gnuplot.Graph{
-		Title: "hyperkit physical footprint",
+		Title: "hyperkit physical footprint vs RSS vs VSZ, 10.14, idle Docker",
 		Lines: []*gnuplot.Line{
-			&footprint,
+			&gnuplot.Line{
+				Label:  "physical footprint",
+				Points: footprintPoints,
+			},
+			&gnuplot.Line{
+				Label:  "Resident Memory (RSS)",
+				Points: RSSPoints,
+			},
+			&gnuplot.Line{
+				Label:  "Virtual Size (VSZ)",
+				Points: VSZPoints,
+			},
 		},
 	}
 	if err := g.Render("/tmp/output.png"); err != nil {
