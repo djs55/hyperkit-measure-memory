1a626a53e443556e0f5f86fd10e936d9c7f305b4
David Scott <dave@recoil.org>
2019-03-03 15:05:18 +0000

    Generate footprint-{hyperkit,docker}.png
    Signed-off-by: David Scott <dave@recoil.org>

--- cmd/analysis/footprint/main.go
+++ cmd/analysis/footprint/main.go
@@ -7,16 +7,27 @@ import (
 	"github.com/djs55/hyperkit-measure-memory/pkg/sample"
 )
 
+var (
+	dir      = "10.14-idle"
+	hyperkit = "com.docker.hyperkit"
+	firefox  = "firefox"
+)
+
 func main() {
-	footprintPoints, err := sample.ReadDir("10.14-idle", func(s sample.Sample) int64 {
+	doHyperkit()
+	doFirefox()
+}
+
+func doHyperkit() {
+	footprintPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
 		return int64(s.Footprint)
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	VSZPoints, err := sample.ReadDir("10.14-idle", func(s sample.Sample) int64 {
+	VSZPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
 		for _, command := range s.PS {
-			if command.Command == "com.docker.hyperkit" {
+			if command.Command == hyperkit {
 				return command.VSZ
 			}
 		}
@@ -25,9 +36,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	RSSPoints, err := sample.ReadDir("10.14-idle", func(s sample.Sample) int64 {
+	RSSPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
 		for _, command := range s.PS {
-			if command.Command == "com.docker.hyperkit" {
+			if command.Command == hyperkit {
 				return command.RSS
 			}
 		}
@@ -53,7 +64,58 @@ func main() {
 			},
 		},
 	}
-	if err := g.Render("/tmp/output.png"); err != nil {
+	if err := g.Render("footprint-docker.png"); err != nil {
+		log.Fatalf("Failed to render: %v", err)
+	}
+}
+
+func doFirefox() {
+	footprintPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
+		return int64(s.FirefoxFootprint)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	VSZPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
+		for _, command := range s.PS {
+			if command.Command == firefox {
+				return command.VSZ
+			}
+		}
+		return int64(0)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	RSSPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
+		for _, command := range s.PS {
+			if command.Command == firefox {
+				return command.RSS
+			}
+		}
+		return int64(0)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	g := gnuplot.Graph{
+		Title: "firefox physical footprint vs RSS vs VSZ, 10.14, idle Docker",
+		Lines: []*gnuplot.Line{
+			&gnuplot.Line{
+				Label:  "physical footprint",
+				Points: footprintPoints,
+			},
+			&gnuplot.Line{
+				Label:  "Resident Memory (RSS)",
+				Points: RSSPoints,
+			},
+			&gnuplot.Line{
+				Label:  "Virtual Size (VSZ)",
+				Points: VSZPoints,
+			},
+		},
+	}
+	if err := g.Render("footprint-firefox.png"); err != nil {
 		log.Fatalf("Failed to render: %v", err)
 	}
 }
