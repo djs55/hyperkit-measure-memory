4476f3bbfde1c430a21222a5080f358554fdba73
David Scott <dave@recoil.org>
2019-03-06 16:03:38 +0000

    Add more analysis tools
    Signed-off-by: David Scott <dave@recoil.org>

---
+++ cmd/analysis/invm-4/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+
+	"github.com/djs55/hyperkit-measure-memory/pkg/gnuplot"
+	"github.com/djs55/hyperkit-measure-memory/pkg/sample"
+)
+
+const (
+	macOS1012 = 0
+	macOS1014 = iota
+)
+
+var (
+	hyperkit = "com.docker.hyperkit"
+)
+
+func main() {
+	doHyperkit(macOS1014)
+}
+
+func doHyperkit(macOS int) {
+	dir := getDir(macOS)
+
+	RSSPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
+		for _, command := range s.PS {
+			if command.Command == hyperkit {
+				return command.RSS
+			}
+		}
+		return int64(0)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := "10.12"
+	if macOS == macOS1014 {
+		m = "10.14"
+	}
+	lines := []*gnuplot.Line{
+		&gnuplot.Line{
+			Label:  "Resident Memory (RSS)",
+			Points: RSSPoints,
+		},
+	}
+	if macOS == macOS1014 {
+		footprintPoints, err := sample.ReadDir(dir, func(s sample.Sample) int64 {
+			return int64(s.Footprint)
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		lines = append(lines, &gnuplot.Line{
+			Label:  "physical footprint",
+			Points: footprintPoints,
+		})
+	}
+
+	g := gnuplot.Graph{
+		Title: "hyperkit memory usage, " + m,
+		Lines: lines,
+	}
+	if err := g.Render("footprint-" + dir + ".png"); err != nil {
+		log.Fatalf("Failed to render: %v", err)
+	}
+}
+
+func getDir(macOS int) string {
+	m := "10.12"
+	if macOS == macOS1014 {
+		m = "10.14"
+	}
+	return m + "-invm-touch-4"
+}
